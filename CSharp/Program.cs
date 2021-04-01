07b0f59db5e03161adf9eecfec8afc4a01f9e1af
Dmitry-Dzyuba <diman.secret@mail.ru>
2021-04-01 21:29:45 +0300

    Using different variations of a switch - with constants, with types, with restrictions when and so on. P147-149

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -18,10 +18,155 @@ namespace CSharp
         static void Main()
         {
             Console.OutputEncoding = Encoding.UTF8;
+            ExecutePatternMatchingSwitchWithWhen();
 
-            ExecuteIfElseUsingConditionalOperator();
+
 
             Console.ReadLine();
+        }
+        static void ExecutePatternMatchingSwitchWithWhen()
+        {
+            Console.WriteLine("1 [C#], 2 [VB]");
+            Console.Write("Please pick your language preference: ");
+            object langChoice = Console.ReadLine();
+            var choice = int.TryParse(langChoice.ToString(), out int c) ? c : langChoice;
+            switch (choice)
+            {
+                case int i when i == 2:
+                case string s when s.Equals("VB", StringComparison.OrdinalIgnoreCase):
+                    Console.WriteLine("VB: OOP, multithreading, and more");
+                    break;
+                case int i when i == 1:
+                case string s when s.Equals("C#", StringComparison.OrdinalIgnoreCase):
+                    Console.WriteLine("Good choice. C# is a fine language");
+                    break;
+                default:
+                    Console.WriteLine("Well... Good luch with that");
+                    break;
+            }
+            /*switch (choice) // так никогда не сработает
+            {
+                case int i:
+                // Делать что-то
+                break;
+                case int i when 1 == 0:
+                    // Делать что-то
+                    break;
+                case int i when i == -1:
+                    // Делать что-то
+                    break;
+            }*/
+        }
+        static void ExecutePatternMatchingSwitch()
+        {
+            Console.WriteLine("1 [Integer (5)], 2 [String (\"Hi\")], 3 [Decimal (2.5)]");
+            Console.Write("Please choose an option: ");
+            string userChoice = Console.ReadLine();
+            object choice;
+            //standart switch operator using comparison with constants
+            switch (userChoice)
+            {
+                case "1":
+                    choice = 5; break;
+                case "2":
+                    choice = "Hi"; break;
+                case "3":
+                    choice = 2.5; break;
+                default:
+                    choice = 5;
+                    break;
+            }
+            switch (choice)
+            {
+                case int i:
+                    Console.WriteLine($"Your choice is integer {i}");
+                    break;
+                case string s:
+                    Console.WriteLine($"Your choice is string {s}");
+                    break;
+                case decimal d:
+                    Console.WriteLine($"Your choice is decimal {d}");
+                    break;
+                default:
+                    Console.WriteLine("Your choice is something else");
+                    break;
+            }
+        }
+        static void SwitchOnEnumExample()
+        {
+            Console.Write("Enter your favourite day of the week: ");
+            DayOfWeek favDay;
+            try
+            {
+                favDay = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Bad input!");
+                return;
+            }
+            switch (favDay)
+            {
+                
+                case DayOfWeek.Monday:
+                    Console.WriteLine("Another day, another dollar.");
+                    break;
+                case DayOfWeek.Tuesday:
+                    Console.WriteLine("At least it's not Monday.");
+                    break;
+                case DayOfWeek.Wednesday:
+                    Console.WriteLine("A fine day.");
+                    break;
+                case DayOfWeek.Thursday:
+                    Console.WriteLine("Almost Friday");
+                    break;
+                case DayOfWeek.Friday:
+                    Console.WriteLine("Yeah. Friday rules");
+                    break;
+                case DayOfWeek.Saturday:
+                case DayOfWeek.Sunday:
+                    Console.WriteLine("Great day indeed");
+                    break;
+                default:
+                    break;
+            }
+        }
+        static void SwitchOnStringExample()
+        {
+            Console.WriteLine("C# or VB");
+            Console.Write("Please pick your language preference: ");
+            string langChoise = Console.ReadLine();
+            switch (langChoise)
+            {
+                case "C#":
+                    Console.WriteLine("Good choice. C# is a fine language");
+                    break;
+                case "VB":
+                    Console.WriteLine("VB: OOP, multithreading, and more");
+                    break;
+                default:
+                    Console.WriteLine("Well... Good luch with that");
+                    break;
+            }
+        }
+        static void SwitchExample()
+        {
+            Console.WriteLine("1 [C#], 2 [VB]. P144");
+            Console.Write("Please pick your language preference: ");
+            string langChoise = Console.ReadLine();
+            int n = int.Parse(langChoise);
+            switch (n)
+            {
+                case 1:
+                    Console.WriteLine("Good choice. C# is a fine language");
+                    break;
+                case 2:
+                    Console.WriteLine("VB: OOP, multithreading, and more");
+                    break;
+                default:
+                    Console.WriteLine("Well... Good luch with that");
+                    break;
+            }
         }
         static void ExecuteIfElseUsingConditionalOperator()
         {
